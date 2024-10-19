e4ec410688e1a38681e960fd526c3940de2e5a1c
Vyacheslav Dubovitskiy <slava@python.ru>
2024-10-19 06:13:33 +0400

    Initial Commit

--- src/OOP/Main.java
+++ src/OOP/Main.java
@@ -4,11 +4,11 @@ public class Main {
     public static void main(String[] args) {
 
         //Авторы книг
-        Author pushkin = new Author("Александр","Пушкин");
-        Author tolstoy = new Author ("Лев", "Толстой");
+        Author pushkin = new Author("Александр", "Пушкин");
+        Author tolstoy = new Author("Лев", "Толстой");
 
         // Книги
-        Book dubrovski = new Book("Дубровкий",pushkin, 1841);
+        Book dubrovski = new Book("Дубровкий", pushkin, 1841);
         Book WarAndPeace = new Book("Война и мир", tolstoy, 1867);
 
         System.out.println(WarAndPeace.BookInfo());
