1d0aef99fb304959852b18faa6cfc8df107806ab
Vyacheslav Dubovitskiy <slava@python.ru>
2024-10-24 20:06:01 +0400

    Initial Commit

--- src/OOP/Author.java
+++ src/OOP/Author.java
@@ -2,34 +2,26 @@ package OOP;
 
 public class Author {
     // Информация об авторе
-    private String firstName;
+    private String firstName;;
     private String lastName;
 
+
+    // Создание пустого конструтора
+    public Author () {
+
+    }
     // Создание обьекта
     public Author(String fName, String lName) {
         this.firstName = fName;
         this.lastName = lName;
     }
 
-    public String getFirstName() {
-        return firstName;
-    }
 
-    public void setFirstName(String fName) {
-        this.firstName = fName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lName) {
-        this.lastName = lName;
-    }
-
-    // Метод, который возвращает полное имя автора в виде строки
-    public String getFullName() {
+    // toString метод для возвращения в виде строки
+    @Override
+    public String toString() {
         return firstName + " " + lastName;
     }
+
 }
 
--- src/OOP/Book.java
+++ src/OOP/Book.java
@@ -14,35 +14,37 @@ public class Book {
     }
 
 
-    public String getTitle() {
-        return title;
+    //переопредление метода в toString
+
+    @Override
+    public String toString() {
+        return "Название: " + title + "\nАвтор: " + author.toString() + "\nГод: " + year;
     }
+
 
     public void setTitle(String title) {
         this.title = title;
-    }
-
-    public Author getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(Author author) {
-        this.author = author;
-    }
-
-    public int getYear() {
-        return year;
     }
 
     public void setYear(int year) {
         this.year = year;
     }
 
-
-    // вывод информации о книге
-    public String BookInfo() {
-        return "Название: " + title + "\nАвтор: " + author.getFullName() + "\nГод: " + year;
-
+    public String getTitle() {
+        return title;
     }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
 }
 
